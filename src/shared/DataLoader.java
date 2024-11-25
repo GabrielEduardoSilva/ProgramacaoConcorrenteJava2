0d15bcd3953a5ab9163a917bf220a3c2c3bc63bd
Victor Inácio <42353254+victor-inacio@users.noreply.github.com>
2024-11-24 22:56:23 -0300

    Implementação da classe DataLoader
    Essa classe é responsável por fazer parse do JSON do dataset e transformá-lo em um ArrayList de instâncias da classe Article
    Isso foi feito para facilitar o manuseio dos dados com o Java

--- src/shared/DataLoader.java
+++ src/shared/DataLoader.java
@@ -1,5 +1,34 @@
 package shared;
 
-public class DataLoader {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import shared.Article;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DataLoader {
+    public static List<Article> loadDataset(String filePath) {
+        List<Article> articles = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            JsonNode rootNode = mapper.readTree(new File(filePath));
+            JsonNode titlesNode = rootNode.path("title");
+
+            titlesNode.fields().forEachRemaining(entry -> {
+                String title = entry.getValue().asText();
+                articles.add(new Article(title));
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao carregar o dataset: " + filePath);
+        }
+
+        return articles;
+    }
 }
