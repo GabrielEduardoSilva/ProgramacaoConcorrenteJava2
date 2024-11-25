85ee18d1239c4974d54186366a173d76f22bfcd2
Victor Inácio <42353254+victor-inacio@users.noreply.github.com>
2024-11-24 22:47:58 -0300

    Implementação da classe Article
    Essa classe representa um artigo e tem apenas uma propriedade que é o title (String)

--- src/shared/Article.java
+++ src/shared/Article.java
@@ -1,4 +1,18 @@
 package shared;
 
 public class Article {
+    private String title;
+
+    public Article(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title;
+    }
 }
