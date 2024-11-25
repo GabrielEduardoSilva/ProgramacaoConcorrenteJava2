7c1bd93c145cf84c3fa7d64230e6eed57a62feab
GabrielEduardoSilva <102189527+GabrielEduardoSilva@users.noreply.github.com>
2024-11-24 23:00:32 -0300

    Implementação do SearchResult
    Implementada a classe SearchResult no pacote shared para representar os resultados da busca por artigos.

--- src/shared/SearchResult.java
+++ src/shared/SearchResult.java
@@ -1,5 +1,35 @@
 package shared;
 
-public class SearchResult {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.List;
+import shared.Article;
+
+public class SearchResult {
+    @JsonProperty("articles")
+    private List<Article> articles;
+
+    public SearchResult() {
+    }
+
+    public SearchResult(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    @Override
+    public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "{}";
+        }
+    }
 }
